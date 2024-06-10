1b15de8b93623b01dd5110e2f918886915bff8ae
RD42 <42702181+dashr9230@users.noreply.github.com>
2024-06-10 23:07:15 +0800

    Add `CPlayerPool::New(...)`

--- server/playerpool.cpp
+++ server/playerpool.cpp
@@ -1,6 +1,11 @@
 
 #include "main.h"
 
+extern CNetGame *pNetGame;
+extern CRcon	*pRcon;
+
+//----------------------------------------------------
+
 CPlayerPool::CPlayerPool()
 {
 	for(BYTE bytePlayerID = 0; bytePlayerID != MAX_PLAYERS; bytePlayerID++) {
@@ -10,12 +15,58 @@ CPlayerPool::CPlayerPool()
 	}
 }
 
+//----------------------------------------------------
+
 CPlayerPool::~CPlayerPool()
 {
 	for(BYTE bytePlayerID = 0; bytePlayerID != MAX_PLAYERS; bytePlayerID++) {
 		Delete(bytePlayerID,0);
 	}
 }
+
+//----------------------------------------------------
+
+BOOL CPlayerPool::New(BYTE bytePlayerID, PCHAR szPlayerName)
+{
+	if(GetSlotState(bytePlayerID) || m_pPlayers[bytePlayerID] || strlen(szPlayerName) < 1)
+	{
+		return FALSE; // Player already deleted or not used.
+	}
+
+	m_pPlayers[bytePlayerID] = new CPlayer();
+
+	if(m_pPlayers[bytePlayerID])
+	{
+		strcpy(m_szPlayerName[bytePlayerID],szPlayerName);
+		m_pPlayers[bytePlayerID]->SetID(bytePlayerID);
+		m_bPlayerSlotState[bytePlayerID] = TRUE;
+		field_190[bytePlayerID] = 0;
+		m_iPlayerScore[bytePlayerID] = 0;
+		m_bIsAnAdmin[bytePlayerID] = FALSE;
+
+		// Notify all the other players of a newcommer with
+		// a 'ServerJoin' join RPC 
+		RakNet::BitStream bsSend;
+		bsSend.Write(bytePlayerID);
+		bsSend.Write(strlen(szPlayerName));
+		bsSend.Write(szPlayerName,strlen(szPlayerName));
+		pNetGame->GetRakServer()->RPC("ServerJoin" ,&bsSend,HIGH_PRIORITY,RELIABLE_ORDERED,0,
+			pNetGame->GetRakServer()->GetPlayerIDFromIndex(bytePlayerID),true,false);
+
+		char ip[22];
+		WORD port;
+		pNetGame->GetRakServer()->GetPlayerIPFromID(pNetGame->GetRakServer()->GetPlayerIDFromIndex(bytePlayerID), ip, &port);
+		pRcon->ConsolePrintf("[join] %u %s %s %d", bytePlayerID, szPlayerName, ip, port);
+
+		return TRUE;
+	}
+	else
+	{
+		return FALSE;
+	}
+}
+
+//----------------------------------------------------
 
 BOOL CPlayerPool::Delete(BYTE bytePlayerID, BYTE byteReason)
 {
--- server/playerpool.h
+++ server/playerpool.h
@@ -10,10 +10,11 @@ private:
 
 	BOOL	m_bPlayerSlotState[MAX_PLAYERS];
 	CPlayer *m_pPlayers[MAX_PLAYERS];
-	int		field_190[MAX_PLAYERS];
-
+	int field_190[50];
+	CHAR	m_szPlayerName[MAX_PLAYERS][MAX_PLAYER_NAME+1];
 	int 	m_iPlayerScore[MAX_PLAYERS];
 	int		m_iPlayerMoney[MAX_PLAYERS];
+	BOOL	m_bIsAnAdmin[MAX_PLAYERS];
 
 public:
 
@@ -22,6 +23,14 @@ public:
 
 	void Process();
 	BOOL Delete(BYTE bytePlayerID, BYTE byteReason);
+	BOOL New(BYTE bytePlayerID, PCHAR szPlayerName);
+
+	// Find out if the slot is inuse.
+	BOOL GetSlotState(BYTE bytePlayerID) {
+		if(bytePlayerID > MAX_PLAYERS) { return FALSE; }
+		return m_bPlayerSlotState[bytePlayerID];
+	};
+
 
 	void ResetPlayerScoresAndMoney() {
 		memset(&m_iPlayerScore[0],0,sizeof(int) * MAX_PLAYERS);
